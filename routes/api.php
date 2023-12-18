febcbbed438435a854558af95cf42a46abb3a5fd
Kevin Nderitu <nderitukelvin19@gmail.com>
2023-12-18 18:18:17 +0300

    Second push

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/user', function (Request $request, Response $response) {
+    // return $request->user();
+    return $response()->json(["test" => 'another'], 200);
 });
